3ce22f4bc9f36f0d67cb68773cb7f998f94fe41c
vanessa <vanessa@adhawkmicrosystems.com>
2022-05-11 14:51:25 -0400

    Start implementing login flow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/compat/app'
+import { firebaseConfig } from './secret';
 
 const app = createApp(App)
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 app.use(router)
 
