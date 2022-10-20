93e442c1e960577f1dcba60ff3396f8f9ba86c52
vanessa <vanessa@adhawkmicrosystems.com>
2022-10-20 19:58:10 -0400

    Redirect to games page for now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
-<script lang="ts">
+<script context="module" land="ts">
+	export async function load() {
+		return {
+			status: 302,
+			redirect: '/games'
+		};
+	}
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<style>
-</style>
