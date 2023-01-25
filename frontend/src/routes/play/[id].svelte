7dea46498c941636c9e38b81f6e9d4ecca29fa15
vanessa <vanessa@adhawkmicrosystems.com>
2023-01-24 21:17:56 -0500

    Refactor for frontend and backend

--- frontend/src/routes/play/[id].svelte
+++ frontend/src/routes/play/[id].svelte
@@ -1,13 +1,9 @@
-<script>
-	import GameBoardPlay from "src/components/GameBoardPlay.svelte";
-
-</script>
 <script context="module" lang="ts">
 	export async function load({ fetch, params }: any) {
 		const gameDataRes = await fetch(`../mocks/gameData/game${params.id}.json`);
 		const gameData = await gameDataRes.json();
 
-		if (gamesRes.ok && gameDataRes.ok) {
+		if (gameData.ok && gameDataRes.ok) {
 			return {
 				props: {
 					gameData: gameData
@@ -18,7 +14,7 @@
 </script>
 
 <script lang="ts">
-    export let gameData: any;
+	// export let gameData: any;
 </script>
 
 <!-- <GameBoardPlay roundData={gameData.rounds} -->
