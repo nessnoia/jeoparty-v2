df68bc702d574d761f0db44fd31b2d4e41bfccdf
vanessa <vanessa@adhawkmicrosystems.com>
2023-01-24 21:15:23 -0500

    Add game play route

---
+++ src/routes/play/[id].svelte
@@ -0,0 +1,24 @@
+<script>
+	import GameBoardPlay from "src/components/GameBoardPlay.svelte";
+
+</script>
+<script context="module" lang="ts">
+	export async function load({ fetch, params }: any) {
+		const gameDataRes = await fetch(`../mocks/gameData/game${params.id}.json`);
+		const gameData = await gameDataRes.json();
+
+		if (gamesRes.ok && gameDataRes.ok) {
+			return {
+				props: {
+					gameData: gameData
+				}
+			};
+		}
+	}
+</script>
+
+<script lang="ts">
+    export let gameData: any;
+</script>
+
+<!-- <GameBoardPlay roundData={gameData.rounds} -->
