ebb497dfef4b131347693d0773dbaa3d5584a6e4
vanessa <vanessa@adhawkmicrosystems.com>
2023-02-26 14:33:13 -0500

    Rename folders to frontend and backend

--- sveltekit/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(client-play)/character/+page.svelte";
--- sveltekit/.svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(host-play)/join/+page.svelte";
