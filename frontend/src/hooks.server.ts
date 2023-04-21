cbcccdac7923ccc153310f903a5fbb46681d93ab
vanessa <vanessa@adhawkmicrosystems.com>
2023-04-21 19:26:05 -0400

    Fix authentication redirecting stopping people from joining and fix colyseus download

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,26 +1,9 @@
 import { connectToDatabase } from "$lib/server/database";
 import mongoose from "mongoose";
 import { auth } from "$lib/server/lucia";
-import { redirect, type Handle } from "@sveltejs/kit";
+import type { Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 import { ATLAS_URI } from "$env/static/private";
-
-const authenticate = (async ({ event, resolve }) => {
-    const session = await event.locals.auth.validate();
-    if (!session && (!event.url.pathname.startsWith('/login') 
-                 && !event.url.pathname.startsWith('/register') 
-                 && !event.url.pathname.startsWith('/api'))) {
-        throw redirect(303, '/login');
-    } else if (session && (event.url.pathname.startsWith('/login') || event.url.pathname.startsWith('/register'))) {
-        throw redirect(303, '/games');
-    }
-     
-    // If the request is still here, just proceed as normally
-    const result = await resolve(event, {
-        transformPageChunk: ({ html }) => html
-    });
-    return result;
-}) satisfies Handle;
 
 mongoose.connect(ATLAS_URI);
 connectToDatabase(ATLAS_URI).then(() => {
@@ -34,5 +17,4 @@ export const handleAuth = (async ({ event, resolve }) => {
 
 export const handle = sequence(
     handleAuth,
-    authenticate,
 );
---
+++ frontend/src/routes/(user-handling)/+layout.server.ts
@@ -0,0 +1,11 @@
+import { auth } from "$lib/server/lucia";
+import { redirect } from "@sveltejs/kit";
+
+export const load = async (event) => {
+    const authRequest = auth.handleRequest(event);
+    const session = await authRequest.validate();
+
+    if (session) {
+        throw redirect(303, '/games');
+    }
+}
