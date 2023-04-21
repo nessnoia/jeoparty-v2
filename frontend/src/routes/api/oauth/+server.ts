c47888197062ed7fbb7978b1d11099ba9fff5c73
vanessa <vanessa@adhawkmicrosystems.com>
2023-04-21 16:00:22 -0400

    Once again removing references to oauth stuff (just commenting in case I decide to fix it)

--- frontend/src/routes/api/oauth/+server.ts
+++ frontend/src/routes/api/oauth/+server.ts
@@ -1,36 +1,36 @@
-import type { RequestHandler } from './$types';
-import { githubAuth, googleAuth } from '$lib/server/lucia';
+// import type { RequestHandler } from './$types';
+// import { githubAuth, googleAuth } from '$lib/server/lucia';
 
-export const GET = (async ({ url, cookies }) => {
-	const provider = url.searchParams.get('provider');
-	if (provider === 'google') {
-		const [url, state] = await googleAuth.getAuthorizationUrl();
-		cookies.set('google_oauth_state', state, {
-			path: '/',
-            httpOnly: true,
-			maxAge: 60 * 60
-		});
-		return new Response(null, {
-			status: 302,
-			headers: {
-				location: url.toString()
-			}
-		});
-	} else if (provider === 'github') {
-        const [url, state] = await githubAuth.getAuthorizationUrl();
-		cookies.set('github_oauth_state', state, {
-			path: '/',
-            httpOnly: true,
-			maxAge: 60 * 60
-		});
-		return new Response(null, {
-			status: 302,
-			headers: {
-				location: url.toString()
-			}
-		});
-    }
-	return new Response(null, {
-		status: 400
-	});
-}) satisfies RequestHandler;
+// export const GET = (async ({ url, cookies }) => {
+// 	const provider = url.searchParams.get('provider');
+// 	if (provider === 'google') {
+// 		const [url, state] = await googleAuth.getAuthorizationUrl();
+// 		cookies.set('google_oauth_state', state, {
+// 			path: '/',
+//             httpOnly: true,
+// 			maxAge: 60 * 60
+// 		});
+// 		return new Response(null, {
+// 			status: 302,
+// 			headers: {
+// 				location: url.toString()
+// 			}
+// 		});
+// 	} else if (provider === 'github') {
+//         const [url, state] = await githubAuth.getAuthorizationUrl();
+// 		cookies.set('github_oauth_state', state, {
+// 			path: '/',
+//             httpOnly: true,
+// 			maxAge: 60 * 60
+// 		});
+// 		return new Response(null, {
+// 			status: 302,
+// 			headers: {
+// 				location: url.toString()
+// 			}
+// 		});
+//     }
+// 	return new Response(null, {
+// 		status: 400
+// 	});
+// }) satisfies RequestHandler;
--- frontend/src/routes/api/oauth/google/+server.ts
+++ frontend/src/routes/api/oauth/google/+server.ts
@@ -1,25 +1,25 @@
-import { auth, googleAuth } from '$lib/server/lucia';
-import { redirect } from '@sveltejs/kit';
-import type { RequestHandler } from '../$types';
+// import { auth, googleAuth } from '$lib/server/lucia';
+// import { redirect } from '@sveltejs/kit';
+// import type { RequestHandler } from '../$types';
 
-export const GET = (async ({ cookies, url, locals }) => {
-	const code = url.searchParams.get('code');
-	const state = url.searchParams.get('state');
-	const storedState = cookies.get('google_oauth_state');
-	if (storedState !== state || !code || !state) throw new Response(null, { status: 401 });
-	try {
-		const { existingUser, providerUser, createUser } = await googleAuth.validateCallback(code);
-		const user =
-			existingUser ??
-			(await createUser({
-				email: providerUser.email
-			}));
-		const session = await auth.createSession(user.userId);
-		locals.auth.setSession(session);
-	} catch (e) {
-		return new Response(null, {
-			status: 500
-		});
-	}
-	throw redirect(302, '/games');
-}) satisfies RequestHandler;
+// export const GET = (async ({ cookies, url, locals }) => {
+// 	const code = url.searchParams.get('code');
+// 	const state = url.searchParams.get('state');
+// 	const storedState = cookies.get('google_oauth_state');
+// 	if (storedState !== state || !code || !state) throw new Response(null, { status: 401 });
+// 	try {
+// 		const { existingUser, providerUser, createUser } = await googleAuth.validateCallback(code);
+// 		const user =
+// 			existingUser ??
+// 			(await createUser({
+// 				email: providerUser.email
+// 			}));
+// 		const session = await auth.createSession(user.userId);
+// 		locals.auth.setSession(session);
+// 	} catch (e) {
+// 		return new Response(null, {
+// 			status: 500
+// 		});
+// 	}
+// 	throw redirect(302, '/games');
+// }) satisfies RequestHandler;
