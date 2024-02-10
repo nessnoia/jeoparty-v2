d9d00fcb15fe498afb5a78c22bc5396e9c56cfa1
vanessa <vanessanoia411@gmail.com>
2024-02-10 11:46:08 -0500

    other client waiting pages ui

--- frontend/src/routes/(play-client)/categories/+page.svelte
+++ frontend/src/routes/(play-client)/categories/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { roomStore } from '$lib/colyseus-client';
 	import Waiting from '$lib/components/play/Waiting.svelte';
@@ -16,7 +15,37 @@
 	}
 </script>
 
-<Waiting>
-	<h1>Showing categories</h1>
-	<h2>Get ready...</h2>
-</Waiting>
+<div id="background">
+	<Waiting size={80}>
+		<h1>Showing categories</h1>
+		<h2>Get ready...</h2>
+	</Waiting>
+</div>
+
+<style>
+	#background {
+		width: 100%;
+		height: 100%;
+		color: var(--white);
+		background-color: var(--black);
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 5%;
+		padding: 0 8%;
+	}
+
+	h1 {
+		margin: 0;
+		font-size: var(--size-4);
+		text-transform: uppercase;
+	}
+
+	h2 {
+		margin: 0;
+		font-size: var(--size-6);
+		text-transform: uppercase;
+	}
+</style>
--- frontend/src/routes/(play-client)/finaljeopartywait/+page.svelte
+++ frontend/src/routes/(play-client)/finaljeopartywait/+page.svelte
@@ -29,6 +29,30 @@
 	}
 </script>
 
-<Waiting>
-	<h1>Waiting for final jeoparty.</h1>
-</Waiting>
+<div id="background">
+	<Waiting size={100}>
+		<h1>Waiting for final jeoparty</h1>
+	</Waiting>
+</div>
+
+<style>
+	#background {
+		width: 100%;
+		height: 100%;
+		color: var(--white);
+		background-color: var(--black);
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 5%;
+		padding: 0 8%;
+	}
+
+	h1 {
+		margin: 0;
+		font-size: var(--size-4);
+		text-transform: uppercase;
+	}
+</style>
