6ef8185b30956e97f3b0110e25a12cb23989c178
vanessa <vanessanoia411@gmail.com>
2024-02-10 14:29:24 -0500

    Make register page look nicer

--- frontend/src/routes/(user-handling)/register/+page.svelte
+++ frontend/src/routes/(user-handling)/register/+page.svelte
@@ -36,7 +36,10 @@
 	h1 {
 		font-size: var(--size-4);
 		margin-top: 0;
+		margin-bottom: 6%;
+		color: var(--black);
 	}
+
 	#container {
 		display: flex;
 		height: 100%;
@@ -45,6 +48,8 @@
 		align-items: center;
 		text-align: center;
 		flex-direction: column;
+		color: var(--black);
+		background-color: var(--white);
 	}
 
 	#register-form {
@@ -53,38 +58,50 @@
 		justify-content: center;
 		align-items: center;
 		text-align: center;
-		width: 40%;
-		min-width: var(--min-width);
+		width: 70%;
+		max-width: 700px;
+		height: 100%;
 	}
 
 	form {
 		display: flex;
 		align-items: stretch;
 		flex-direction: column;
+		justify-content: center;
+		width: 100%;
 		gap: 12px;
-		width: 80%;
 	}
 
 	.form-item {
 		display: flex;
-		flex-direction: row;
 		justify-content: space-between;
 		align-items: center;
+		text-align: left;
+		gap: 2%;
+		font-size: var(--size-9);
 	}
 
 	.form-item input {
+		background-color: transparent;
+		padding: 1% 3%;
+		border-radius: 5px;
+		border: 1px solid var(--black);
 		width: 70%;
-		padding: 3px 5px;
+		font-size: var(--size-8);
 	}
 
 	#register {
-		padding: 5px;
-		margin-bottom: 20px;
-		margin-top: 5px;
-		font-size: var(--size-10);
+		padding: 3%;
+		margin: 3% 0 4% 0;
+		font-size: var(--size-8);
+		font-weight: bold;
+		border: none;
+		color: var(--white);
+		background-color: var(--primary-600);
+		border-radius: 5px;
 	}
 
 	.login {
-		margin-top: 5px;
+		font-size: var(--size-9);
 	}
 </style>
